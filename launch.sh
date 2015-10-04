8bf4b7a629e0b494df196438e7483ee2be7b7bd6
Malhoura <malhoura@hawk.iit.edu>
2015-10-04 10:44:31 -0500

    launch two instances script

---
+++ launch.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+aws ec2 run-instances --image-id ami-d05e75b8 --count 2 --instance-type t2.micro --key-name itmo-444-virtualbox --security-group-ids sg-37695650 --subnet-id subnet-7f4e4708 --associate-public-ip-address --user-data file://install-env.sh --debug 
+
