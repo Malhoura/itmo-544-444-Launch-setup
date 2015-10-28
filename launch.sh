ebb9cecc0f1a8121e658792b0280dba6b57d3962
Malhoura <malhoura@hawk.iit.edu>
2015-10-28 11:41:28 -0500

    load balancer created

--- launch.sh
+++ launch.sh
@@ -6,4 +6,8 @@ mapfile -t myInsARRAY < <(aws ec2 run-instances --image-id ami-d05e75b8 --count 
 echo ${myInsARRAY[@]}
 
 aws ec2 wait instance-running --instance-ids ${myInsARRAY[@]}
-echo "instances are running" 
+echo "instances are running"
+
+#create load balancer
+ELBURL=('aws elb create-load-balancer --load-balancer-name $2 --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,output=text'); 
+echo $ELBURL 
