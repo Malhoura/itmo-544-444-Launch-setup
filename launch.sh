a516af54f9ae0bc1514d5eee3d0bf513107b985b
Malhoura <malhoura@hawk.iit.edu>
2015-10-28 11:36:16 -0500

    array created

--- launch.sh
+++ launch.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-mapfile -t myInsARRAY < <{aws ec2 run-instances --image-id ami-d05e75b8 --count i --instance-type t2.micro --key-name itmo-444-virtualbox --security-group-ids sg-37695650 --subnet-id subnet-7f4e4708 --associate-public-ip-address --iam-instance-profile Name=Mazen-AlHourani --user-data file://itmo-544-444-Environment-setup/install-env.sh --output table | grep InstanceId | sed "s/|//g" | tr -d ' ' | sed "s/InstanceId//g"} 
+declare -a myInsARRAY
+mapfile -t myInsARRAY < <(aws ec2 run-instances --image-id ami-d05e75b8 --count i --instance-type t2.micro --key-name itmo-444-virtualbox --security-group-ids sg-37695650 --subnet-id subnet-7f4e4708 --associate-public-ip-address --iam-instance-profile Name=Mazen-AlHourani --user-data file://itmo-544-444-Environment-setup/install-env.sh --output table | grep InstanceId | sed "s/|//g" | tr -d ' ' | sed "s/InstanceId//g")
+
+echo ${myInsARRAY[@]}
+
+aws ec2 wait instance-running --instance-ids ${myInsARRAY[@]}
+echo "instances are running" 
