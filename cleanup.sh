da503804dd1421c11e009297befdc996ba8a2d25
Malhoura <malhoura@hawk.iit.edu>
2015-11-02 22:02:27 -0600

    commented lines

--- cleanup.sh
+++ cleanup.sh
@@ -61,10 +61,10 @@ aws autoscaling delete-auto-scaling-group --auto-scaling-group-name $SCALENAME
 
 aws autoscaling delete-launch-configuration --launch-configuration-name $LAUNCHCONF
 
-aws autoscaling update-auto-scaling-group --auto-scaling-group-name $SCALENAME --min-size 0 --max-size 0
+#aws autoscaling update-auto-scaling-group --auto-scaling-group-name $SCALENAME --min-size 0 --max-size 0
 
 aws autoscaling delete-auto-scaling-group --auto-scaling-group-name $SCALENAME
-aws autoscaling delete-launch-configuration --launch-configuration-name $LAUNCHCONF
+#aws autoscaling delete-launch-configuration --launch-configuration-name $LAUNCHCONF
 fi
 
 
