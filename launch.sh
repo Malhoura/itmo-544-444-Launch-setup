9fad2f9d739291adb433c635ffed680eef5942c2
Malhoura <malhoura@hawk.iit.edu>
2015-11-04 15:19:37 -0600

    php file path corrected

--- launch.sh
+++ launch.sh
@@ -30,8 +30,10 @@ aws autoscaling create-launch-configuration --launch-configuration-name itmo-544
 aws autoscaling create-auto-scaling-group --auto-scaling-group-name itmo-544-444-autoscaling-group --launch-configuration-name itmo-544-444-launch-config --load-balancer-names $2 --health-check-type ELB --min-size 3 --max-size 6 --desired-capacity 3 --default-cooldown 600 --health-check-grace-period 120 --vpc-zone-identifier subnet-7f4e4708 
 
 #create database subnet groups
-aws rds-create-db-subnet-group --db-subnet-group-name mp1 --db-subnet-group-discreption "group for mp1" --db-subnet-list subnet-7f4e4708, subnet-afa282f6 
+aws rds create-db-subnet-group --db-subnet-group-name mp1 --db-subnet-group-discreption "group for mp1" --subnet-ids subnet-7f4e4708 subnet-afa282f6 
+
 
 aws rds create-db-instance --db-instance-identifier malhoura-mp1 --db-instance-class db.t1.micro --engine MySQL --master-username malhoura --master-user-password malhoura --allocated-storage 10 --db-subnet-group-name mp1 
 
-#php ./setup.php
+aws rds wait db-instance-available --db-instance-identifier malhoura-mp1 
+php ../itmo-544-444-Application-setup/setup.php
