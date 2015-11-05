34a4ccd5b4d970e87a7501e690fe72bb7dbca2bc
Malhoura <malhoura@hawk.iit.edu>
2015-11-05 02:00:22 -0600

    working db instance

--- launch.sh
+++ launch.sh
@@ -4,7 +4,7 @@
 
 #declaring an array in bash
 declare -a myInsARRAY
-mapfile -t myInsARRAY < <(aws ec2 run-instances --image-id ami-d05e75b8 --count $1 --instance-type t2.micro --key-name itmo-444-virtualbox --security-group-ids sg-37695650 --subnet-id subnet-7f4e4708 --associate-public-ip-address --iam-instance-profile Name=phpRole --user-data file://install-webserver.sh --output table | grep InstanceId | sed "s/|//g" | tr -d ' ' | sed "s/InstanceId//g")
+mapfile -t myInsARRAY < <(aws ec2 run-instances --image-id ami-d05e75b8 --count $1 --instance-type t2.micro --key-name itmo-444-virtualbox --security-group-ids sg-37695650 --subnet-id subnet-afa282f6 --associate-public-ip-address --iam-instance-profile Name=phpRole --user-data file://install-webserver.sh --output table | grep InstanceId | sed "s/|//g" | tr -d ' ' | sed "s/InstanceId//g")
 
 #Displaying the created array contents
 echo ${myInsARRAY[@]}
@@ -14,8 +14,16 @@ sudo aws ec2 wait instance-running --instance-ids ${myInsARRAY[@]}
 echo "instances are running"
 
 #create load balancer
-ELBURL=(`aws elb create-load-balancer --load-balancer-name $2 --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 --security-groups sg-37695650 --subnets subnet-7f4e4708 --output=text`); 
+ELBURL=(`aws elb create-load-balancer --load-balancer-name $2 --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 --security-groups sg-37695650 --subnets subnet-afa282f6 --output=text`); 
 echo $ELBURL
+
+echo -e "\nFinished launching ELB and waiting 25 seconds"
+
+echo -e "\n"
+
+for i in {0..25};do echo -ne '.';sleep 1;done
+
+echo -e "\n"
 
 #regiter load balancer
 aws elb register-instances-with-load-balancer --load-balancer-name $2 --instances ${MyInsARRAY[@]}
@@ -27,13 +35,13 @@ aws elb configure-health-check --load-balancer-name $2 --health-check Target=HTT
 aws autoscaling create-launch-configuration --launch-configuration-name itmo-544-444-launch-config --image-id ami-d05e75b8 --key-name itmo-444-virtualbox --security-groups sg-37695650 --instance-type t2.micro --user-data file://install-webserver.sh --iam-instance-profile phpRole 
 
 #create autoscaling group
-aws autoscaling create-auto-scaling-group --auto-scaling-group-name itmo-544-444-autoscaling-group --launch-configuration-name itmo-544-444-launch-config --load-balancer-names $2 --health-check-type ELB --min-size 3 --max-size 6 --desired-capacity 4 --default-cooldown 600 --health-check-grace-period 120 --vpc-zone-identifier subnet-7f4e4708 
+aws autoscaling create-auto-scaling-group --auto-scaling-group-name itmo-544-444-autoscaling-group --launch-configuration-name itmo-544-444-launch-config --load-balancer-names $2 --health-check-type ELB --min-size 3 --max-size 6 --desired-capacity 3 --default-cooldown 600 --health-check-grace-period 120 --vpc-zone-identifier subnet-afa282f6 
 
 #create database subnet groups
-sudo aws rds create-db-subnet-group --db-subnet-group-name mp1 --db-subnet-group-description "group for mp1" --subnet-ids subnet-7f4e4708 subnet-afa282f6  
+sudo aws rds create-db-subnet-group --db-subnet-group-name ITMO444 --db-subnet-group-description "group for mp1" --subnet-ids subnet-7f4e4708 subnet-afa282f6  
 
 
-sudo aws rds create-db-instance --db-name users --db-instance-identifier malhoura-mp1 --db-instance-class db.t1.micro --engine MySQL --master-username malhoura --master-user-password malhoura --allocated-storage 5 --vpc-security-group-ids sg-37695650 --db-subnet-group-name ITMO444 --publicly-accessible 
+sudo aws rds create-db-instance --db-name users --db-instance-identifier malhoura-mp1 --db-instance-class db.t2.micro --engine MySQL --master-username malhoura --master-user-password malhoura --allocated-storage 10 --vpc-security-group-ids sg-37695650 --db-subnet-group-name ITMO444 --publicly-accessible 
 
-sudo aws rds wait db-instance-available --db-instance-identifier malhoura-mp1 
-php ../itmo-544-444-Application-setup/setup.php
+aws rds wait db-instance-available --db-instance-identifier malhoura-mp1 
+#php ../itmo-544-444-Application-setup/setup.php
