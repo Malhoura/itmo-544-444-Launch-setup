13635c75ea16affbfe6a8f9b3aca53aeaa9e9b3b
malhoura <malhoura@hawk.iit.edu>
2015-12-03 10:23:48 -0600

    fixed

--- install-webserver.sh
+++ install-webserver.sh
@@ -4,7 +4,7 @@ sudo apt-get update -y
 sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql php5-imagick
 curl -sS https://getcomposer.org/installer | sudo php &> /tmp/getcomposer.txt
 sudo php composer.phar require aws/aws-sdk-php &> /tmp/runcomposer.txt
-cd -R vendor/ /var/www/html
+cp -R vendor/ /var/www/html
 git clone https://github.com/Malhoura/itmo-544-444-Application-setup.git
 mv ./itmo-544-444-Application-setup/images /var/www/html/images
 mv ./itmo-544-444-Application-setup/index.html /var/www/html
