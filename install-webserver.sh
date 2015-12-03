e2c940154d2a2b34df0726681088cb2937328b4b
malhoura <malhoura@hawk.iit.edu>
2015-12-02 19:31:56 -0600

    a

--- install-webserver.sh
+++ install-webserver.sh
@@ -6,9 +6,10 @@ curl -sS https://getcomposer.org/installer | sudo php &> /tmp/getcomposer.txt
 sudo php composer.phar require aws/aws-sdk-php &> /tmp/runcomposer.txt
 cd -R vendor/ /var/www/html
 git clone https://github.com/Malhoura/itmo-544-444-Application-setup.git
-
+mv ./itmo-544-444-Application-setup/images /var/www/html/images
+mv ./itmo-544-444-Application-setup/index.html /var/www/html
+mv ./itmo-544-444-Application-setup/*.php /var/www/html
 cp -R itmo-544-444-Application-setup/* /var/www/html/
-
 sudo php /var/www/html/setup.php &> /tmp/database-setup.txt
 chmod 600 /var/www/html/setup.php
 mkdir /var/www/html/uploads
