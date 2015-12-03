f5a910705a4fdbc0b4c3c65b5f723c1366086f30
malhoura <malhoura@hawk.iit.edu>
2015-12-03 10:54:57 -0600

    changed permession

--- install-webserver.sh
+++ install-webserver.sh
@@ -13,5 +13,5 @@ cp -R itmo-544-444-Application-setup/* /var/www/html/
 sudo php /var/www/html/setup.php &> /tmp/database-setup.txt
 chmod 600 /var/www/html/setup.php
 mkdir /var/www/html/uploads
-chmod 777 /var/www/html/uploads
+chmod 755 /var/www/html/uploads
 rm /var/www/html/index.html
