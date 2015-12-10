720c639c1e45f9450428f6fa087baac4391ccc1d
malhoura <malhoura@hawk.iit.edu>
2015-12-10 13:34:58 -0600

    test

--- install-webserver.sh
+++ install-webserver.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql php5-imagick
+sudo apt-get install -y php5 php5-imagick apache2 git php5-curl mysql-client curl php5-mysql 
+sudo service apache2 reload
+sudo apt-get install libmagickwand-dev
 curl -sS https://getcomposer.org/installer | sudo php &> /tmp/getcomposer.txt
 sudo php composer.phar require aws/aws-sdk-php &> /tmp/runcomposer.txt
 cp -R vendor/ /var/www/html
@@ -11,7 +13,8 @@ mv ./itmo-544-444-Application-setup/index.html /var/www/html
 mv ./itmo-544-444-Application-setup/*.php /var/www/html
 cp -R itmo-544-444-Application-setup/* /var/www/html/
 sudo php /var/www/html/setup.php &> /tmp/database-setup.txt
-chmod 600 /var/www/html/setup.php
+chmod 755 /var/www/html/setup.php
 mkdir /var/www/html/uploads
 chmod 755 /var/www/html/uploads
+chmod 777 /var/www/html/uploads
 rm /var/www/html/index.html
